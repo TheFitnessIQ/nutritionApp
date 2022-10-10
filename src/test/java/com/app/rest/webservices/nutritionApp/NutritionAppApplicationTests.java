0a9ec339cf21d15faaad9b1ec2dd38be006384ae
Rajib Sahoo <rajibborn2win@gmail.com>
2022-10-10 15:42:39 +0530

    Initial commit

---
+++ src/test/java/com/app/rest/webservices/nutritionApp/NutritionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rest.webservices.nutritionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
